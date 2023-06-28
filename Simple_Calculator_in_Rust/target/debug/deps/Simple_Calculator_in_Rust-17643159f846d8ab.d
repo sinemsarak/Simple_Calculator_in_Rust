c2220548ca7cdc7ff7cda8da107455dcf34c6170
sinemsarak <snmsrkss@gmail.com>
2023-06-29 02:51:05 +0300

    setting up file

---
+++ Simple_Calculator_in_Rust/target/debug/deps/Simple_Calculator_in_Rust-17643159f846d8ab.d
@@ -0,0 +1,5 @@
+C:\Users\sinem\Desktop\github locals\Simple_Calculator_in_Rust\Simple_Calculator_in_Rust\target\debug\deps\Simple_Calculator_in_Rust-17643159f846d8ab.rmeta: src\main.rs
+
+C:\Users\sinem\Desktop\github locals\Simple_Calculator_in_Rust\Simple_Calculator_in_Rust\target\debug\deps\Simple_Calculator_in_Rust-17643159f846d8ab.d: src\main.rs
+
+src\main.rs:
---
+++ Simple_Calculator_in_Rust/target/debug/deps/Simple_Calculator_in_Rust-eddb6e445a0a690b.d
@@ -0,0 +1,5 @@
+C:\Users\sinem\Desktop\github locals\Simple_Calculator_in_Rust\Simple_Calculator_in_Rust\target\debug\deps\Simple_Calculator_in_Rust-eddb6e445a0a690b.rmeta: src\main.rs
+
+C:\Users\sinem\Desktop\github locals\Simple_Calculator_in_Rust\Simple_Calculator_in_Rust\target\debug\deps\Simple_Calculator_in_Rust-eddb6e445a0a690b.d: src\main.rs
+
+src\main.rs:
