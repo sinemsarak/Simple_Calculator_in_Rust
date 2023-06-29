29a45445b3194a25b49f1e1b73a9082d012954aa
sinemsarak <snmsrkss@gmail.com>
2023-06-29 17:03:00 +0300

    modified main

--- Simple_Calculator_in_Rust/src/main.rs
+++ Simple_Calculator_in_Rust/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+#[derive(Debug)]
+
 enum Operation {
     Add {x:f64, y:f64,},
     Subtract {x:f64, y:f64},
@@ -6,31 +8,35 @@ enum Operation {
     Multiply {x:f64, y:f64},
 }
 
-fn calculate (calculation: Operation, x: f64, y:f64) ->  f64 {
+fn calculate (calculation: Operation) ->  f64 {
     match calculation {
 
-        Operation::Add {x, y} => x+y,
- 
+        Operation::Add {x, y} => {
+        return x+y;
+        }
         Operation::Divide {x, y} => { 
             if y != 0.0{
-                x/y
+                return x/y;
             } else {
-                f64::NAN
+                return f64::NAN;
             }
         }
-        Operation::Multiply {x, y} => x*y,
-        Operation::Subtract { x, y } => x-y,
-    }       
+        Operation::Multiply {x, y} => {
+            return x*y;
+        }
+        Operation::Subtract { x, y } => {
+            return x-y;
+        }       
 
+    }
 }
 
 fn main() {
 
-    println!("Enter the first number");
-
-
-    println!("Enter the operation");
-    
-    println!("Enter the second number");
-
+    let x = 1.0;
+    let y = 2.0;
+    let user_operation = Operation::Divide {x, y};
+    println!("{:?}", user_operation);
+    let result = calculate(user_operation);
+    println!("{:?}", result);
 }
