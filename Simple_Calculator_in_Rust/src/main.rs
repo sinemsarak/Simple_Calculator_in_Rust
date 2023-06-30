1e13d73c10f783f06ffd9636e984c4445a757f0e
sinemsarak <snmsrkss@gmail.com>
2023-06-30 18:14:54 +0300

    added getting input from user

--- Simple_Calculator_in_Rust/src/main.rs
+++ Simple_Calculator_in_Rust/src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-#[derive(Debug)]
 
 enum Operation {
     Add {x:f64, y:f64,},
@@ -32,11 +31,47 @@ fn calculate (calculation: Operation) ->  f64 {
 }
 
 fn main() {
+    println!("Enter an operation to calculate (e.g: 3 + 5): ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Read error");
 
-    let x = 1.0;
-    let y = 2.0;
-    let user_operation = Operation::Divide {x, y};
-    println!("{:?}", user_operation);
-    let result = calculate(user_operation);
-    println!("{:?}", result);
-}
+    
+    let mut index = 0;
+
+    while &input[index..index+1] != " " {
+        index=index+1;
+    }
+    let x_str = &input[0..index];
+    let user_operation = &input[index+1..index+2];
+    let y_str = &input[index+3..input.len()];
+
+    let x:f64 = x_str.trim().parse().expect("Invalid input. Please check the example and try again.");
+    let y:f64 = y_str.trim().parse().expect("Invalid input. Please check the example and try again.");
+    let answer:f64;
+    match user_operation {
+        "+" => {
+            answer = calculate(Operation::Add { x, y });
+        }
+        "-" => {
+            answer = calculate(Operation::Subtract { x, y });
+        }
+        "*" => {
+            answer = calculate(Operation::Multiply { x, y });
+        }
+        "/" => {
+            
+            if y == 0.0 {
+                println!("You can not divide a number by zero");
+                return;
+            
+            } else {
+                answer = calculate(Operation::Divide { x, y });
+            }    
+        }
+        _ => {
+            println!("Invalid input. Please check the example and try again.");
+            return;
+        }
+    }
+    println!("Here is the result: {}", answer);
+}    
